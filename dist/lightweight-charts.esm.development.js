f0dace2dc8f4f6e0744fbf14e572d6ce589bfdd7
Patrik <pmauchle@gmail.com>
2022-03-09 21:24:21 +0100

    dist

--- dist/lightweight-charts.esm.development.js
+++ dist/lightweight-charts.esm.development.js
@@ -1,6 +1,6 @@
 /*!
  * @license
- * TradingView Lightweight Charts v4.0.0-dev+202203090750
+ * TradingView Lightweight Charts v4.0.0-dev+202203092024
  * Copyright (c) 2022 TradingView, Inc.
  * Licensed under Apache License 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
@@ -11874,7 +11874,7 @@ function createChart(container, options) {
  * Returns the current version as a string. For example `'3.3.0'`.
  */
 function version() {
-    return "4.0.0-dev+202203090750";
+    return "4.0.0-dev+202203092024";
 }
 
 export { ColorType, CrosshairMode, LastPriceAnimationMode, LineStyle, LineType, MismatchDirection, PriceLineSource, PriceScaleMode, TickMarkType, TrackingModeExitMode, createChart, isBusinessDay, isUTCTimestamp, version };
